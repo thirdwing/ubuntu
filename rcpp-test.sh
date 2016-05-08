adc7eae37be3e50da129deb6ae85c8b00085871b
Qiang Kou <qkou@umail.iu.edu>
2016-05-08 17:00:57 -0400

    update

--- rcpp-test.sh
+++ rcpp-test.sh
@@ -4,6 +4,11 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 apt-get update -q
 apt-get -y install r-base-dev
 apt-get -y install git
-add-apt-repository -y ppa:edd/misc
-apt-get update -q
-apt-get -y install r-cran-runit r-cran-inline r-cran-rbenchmark r-cran-highlight r-cran-pkgkitten
+Rscript -e "install.packages(c('RUnit','highlight','inline','rbenchmark','pkgKitten'), repo = 'https://cran.rstudio.com')"
+cd 
+mkdir .R
+echo "CFLAGS +=             -Wall -pipe -pedantic -std=gnu99" >> .R/Makevars
+echo "CXXFLAGS +=           -Wall -pipe -Wno-unused -pedantic" >> .R/Makevars
+git clone https://github.com/RcppCore/Rcpp.git
+R CMD build --no-build-vignettes --no-manual Rcpp
+R CMD check --no-build-vignettes --no-manual --as-cran Rcpp_*
